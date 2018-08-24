e71058477e81facdf8aed859b03b2179b8b648aa
yoshimi.mitani <miitaanii@gamil.com>
2018-08-24 13:21:46 +0000

    fix migrations

--- db/migrate/20180822215702_add_status_to_tasks.rb
+++ db/migrate/20180822215702_add_status_to_tasks.rb
@@ -1,5 +1,5 @@
 class AddStatusToTasks < ActiveRecord::Migration[5.0]
   def change
-    add_column :tasks, :title, :string
+    add_column :tasks, :status, :string
   end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -17,7 +17,6 @@ ActiveRecord::Schema.define(version: 20180822215702) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.string   "status"
-    t.string   "title"
   end
 
 end
