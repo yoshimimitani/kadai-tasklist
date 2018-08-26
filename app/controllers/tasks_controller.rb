150553961d344f948717a0953d484d6d588facc2
yoshimi.mitani <miitaanii@gamil.com>
2018-08-26 11:52:33 +0000

    retry

--- app/controllers/tasks_controller.rb
+++ app/controllers/tasks_controller.rb
@@ -1,6 +1,6 @@
 class TasksController < ApplicationController
   before_action :require_user_logged_in
-  before_action :correct_user, only: [:destroy,:update,:edit]
+  before_action :correct_user, only: [:destroy,:update,:edit,:show]
   
   def index
   end
